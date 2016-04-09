d30a196e964555d901bac9eb0dae4faff964d921
Ralph Marvin <ralphmarvin4@gmail.com>
2016-04-09 01:24:00 +0100

    Added files via upload

--- IStorageFileOperations.cs
+++ IStorageFileOperations.cs
@@ -11,44 +11,38 @@
         void CreateMainPath();
 
         /// <summary>
-        /// Returns the appropriate file path to store either session data or local storage data.
-        /// </summary>
-        /// <param name="path"></param>
-        string HandleStoragePaths(bool localStorage);
-
-        /// <summary>
         /// Write to a storage file.
         /// </summary>
         /// <param name="fileName"></param>
         /// <param name="content"></param>
-        void WriteToFile(string fileName, string content, bool localStorage);
+        void WriteToFile(string fileName, string content, StorageFileHandler.StorageType storageType);
 
         /// <summary>
         /// Overwrites a storage file.
         /// </summary>
         /// <param name="fileName"></param>
         /// <param name="content"></param>
-        void OverWriteFile(string fileName, string content, bool localStorage);
+        void OverWriteFile(string fileName, string content, StorageFileHandler.StorageType storageType);
         
         /// <summary>
-        /// Checks whether  a storage area is empty or not
+        /// Checks whether a storage area is empty or not
         /// </summary>
         /// <param name="path"></param>
         /// <returns></returns>
-        bool IsEmpty(string path, bool localStorage);
+        bool IsEmpty(string path, StorageFileHandler.StorageType storageType);
 
         /// <summary>
         /// Returns the number of items stored in the storage file
         /// </summary>
         /// <param name="path"></param>
         /// <returns></returns>
-        int Count(string path, bool localStorage);
+        int Count(string path, StorageFileHandler.StorageType storageType);
 
         /// <summary>
         /// Deletes all storage file belonging to a specific application.
         /// </summary>
         /// <param name="path"></param>
-        void DeleteAllFiles(string path, bool localStorage);
+        void DeleteAllFiles(string path, StorageFileHandler.StorageType storageType);
 
         /// <summary>
         /// Checks whether a storage file exists.
@@ -62,19 +56,19 @@
         /// </summary>
         /// <param name="dirName"></param>
         /// <returns></returns>
-        bool DirectoryExists(string dirName, bool localStorage);
+        bool DirectoryExists(string dirName, StorageFileHandler.StorageType storageType);
 
         /// <summary>
         /// Deletes a storage file
         /// </summary>
         /// <param name="fileName"></param>
-        void DeleteFile(string fileName, bool localStorage);
+        void DeleteFile(string fileName, StorageFileHandler.StorageType storageType);
 
         /// <summary>
         /// Reads and returns the content of a storage file.
         /// </summary>
         /// <param name="fileName"></param>
         /// <returns></returns>
-        string ReadFile(string fileName, bool localStorage);
+        string ReadFile(string fileName, StorageFileHandler.StorageType storageType);
     }
 }
